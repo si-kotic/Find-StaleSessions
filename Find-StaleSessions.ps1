173643b7a5b75bf994f050d78788e1dfdcb69f25
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-10-22 22:23:36 +0100

    Initial Commit

---
+++ Find-StaleSessions.ps1
@@ -0,0 +1,16 @@
+Param (
+    $UserName,
+    $SessionAgeLimit,
+    $ComputerName
+)
+quser /server:$ComputerName | ForEach-Object {
+    $_ -replace "SESSIONNAME","" -replace "rdp-tcp#\d\d","" -replace "Disc","Disconnected" -match '^\s([\w\.]+)\s+(\d+)\s+(\w+)\s+([\d:+]+)\s+([\d\/\s:]+)$' | Out-Null
+    $Report = "" | Select-Object UserName,State,IdleTime,LogonTime
+    $Report.UserName = $matches[1]
+    $Report.State = $matches[3]
+    $Report.IdleTime = $matches[4] -replace "\+"," Days, " -replace ":"," Hours and " -replace '$'," Minutes"
+    $Report.LogonTime = $matches[5]
+    $Report
+} | Where-Object {
+    $_.State -eq "Disconnected" -and $_.UserName -eq $UserName -and (((get-date) - (get-date $_.LogonTime)).Days -ge $SessionAgeLimit)
+}
