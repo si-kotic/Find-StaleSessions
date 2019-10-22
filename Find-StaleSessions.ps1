c15cdd4003fee4b895a5597ed206359cbdf4af03
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-10-22 22:38:26 +0100

    Added ComputerName to output

--- Find-StaleSessions.ps1
+++ Find-StaleSessions.ps1
@@ -5,7 +5,8 @@ Param (
 )
 quser /server:$ComputerName | ForEach-Object {
     $_ -replace "SESSIONNAME","" -replace "rdp-tcp#\d\d","" -replace "Disc","Disconnected" -match '^\s([\w\.]+)\s+(\d+)\s+(\w+)\s+([\d:+]+)\s+([\d\/\s:]+)$' | Out-Null
-    $Report = "" | Select-Object UserName,State,IdleTime,LogonTime
+    $Report = "" | Select-Object ComputerName,UserName,State,IdleTime,LogonTime
+    $Report.ComputerName = $ComputerName
     $Report.UserName = $matches[1]
     $Report.State = $matches[3]
     $Report.IdleTime = $matches[4] -replace "\+"," Days, " -replace ":"," Hours and " -replace '$'," Minutes"
