03f2ed54a2f9745562c50a366a9412d7973f585c
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-10-25 23:06:30 +0100

    Handle the console session too

--- Find-StaleSessions.ps1
+++ Find-StaleSessions.ps1
@@ -6,7 +6,7 @@ Param (
 $i = 0
 quser /server:$ComputerName | ForEach-Object {
     IF ($i -ne 0) {
-        $_ -replace "SESSIONNAME","" -replace "rdp-tcp#\d\d","" -replace "Disc","Disconnected" -match '^[\s>]([\w\.]+)\s+(\d+)\s+(\w+)\s+([\d:+"none"\.]+)\s+([\d\/\s:]+)$' | Out-Null
+        $_ -replace "SESSIONNAME","" -replace "rdp-tcp#\d\d","" -replace "console","" -replace "Disc","Disconnected" -match '^[\s>]([\w\.]+)\s+(\d+)\s+(\w+)\s+([\d:+"none"\.]+)\s+([\d\/\s:]+)$' | Out-Null
         $Report = "" | Select-Object ComputerName,UserName,State,IdleTime,LogonTime
         $Report.ComputerName = $ComputerName
         $Report.UserName = $matches[1]
